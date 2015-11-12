be3ec4555338c01ed39c5eee641f811b6db2b898
Ole Ersoy <ole.ersoy@gmail.com>
2015-11-11 19:41:03 -0600

    Added postcss-each

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,11 +4,12 @@ var postcss_import = require('postcss-import');
 var postcss_calc = require('postcss-calc');
 var postcss_custom_properties = require('postcss-custom-properties');
 var postcss_color_function = require('postcss-color-function');
+var postcss_each = require('postcss-each');
 var autoprefixer = require('autoprefixer');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [postcss_import, autoprefixer, postcss_calc, postcss_custom_properties, postcss_color_function];
+var processors = [postcss_import, postcss_each, autoprefixer, postcss_calc, postcss_custom_properties, postcss_color_function];
 
 gulp.task('build:css', function() {
   return gulp.src(PLI.SRC_MAIN_CSS)
