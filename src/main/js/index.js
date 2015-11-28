871b51e95c8fe452e279dabe11d04977fbeed564
Ole Ersoy <ole.ersoy@gmail.com>
2015-11-27 20:54:39 -0600

    Added stylelint and reporting

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -5,11 +5,15 @@ var postcss_calc = require('postcss-calc');
 var postcss_custom_properties = require('postcss-custom-properties');
 var postcss_color_function = require('postcss-color-function');
 var postcss_each = require('postcss-each');
+var postcss_reporter = require('postcss-reporter');
 var autoprefixer = require('autoprefixer');
+var stylelint = require('stylelint');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [postcss_import, postcss_each, autoprefixer, postcss_calc, postcss_custom_properties, postcss_color_function];
+var processors = [stylelint, postcss_import, postcss_each, autoprefixer, postcss_calc, postcss_custom_properties, postcss_color_function, postcss_reporter({
+  clearMessages: true
+})];
 
 gulp.task('build:css', function() {
   return gulp.src(PLI.SRC_MAIN_CSS)
