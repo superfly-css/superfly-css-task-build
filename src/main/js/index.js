a0de81146d07594434fd326bb82baa69e8fa0b47
Ole Ersoy <ole.ersoy@gmail.com>
2017-02-12 15:52:48 -0600

    Removed linting from this task - added it to superfly-css-task-lint

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -7,12 +7,10 @@ var postcss_color_function = require('postcss-color-function');
 var postcss_each = require('postcss-each');
 var postcss_reporter = require('postcss-reporter');
 var autoprefixer = require('autoprefixer');
-var stylelint = require('stylelint');
-var stylelint_config = require('stylelint-config-standard');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [postcss_import, postcss_each, autoprefixer, postcss_custom_properties, postcss_calc, postcss_color_function, stylelint(stylelint_config), postcss_reporter({
+var processors = [postcss_import, postcss_each, autoprefixer, postcss_custom_properties, postcss_calc, postcss_color_function, postcss_reporter({
   clearMessages: true
 })];
 
