7ab221beb496126d97e629d446cbc96af09f9edd
Ole Ersoy <ole.ersoy@gmail.com>
2017-02-23 19:40:34 -0600

    Added apply plugin

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -5,12 +5,13 @@ var postcss_calc = require('postcss-calc');
 var postcss_custom_properties = require('postcss-custom-properties');
 var postcss_color_function = require('postcss-color-function');
 var postcss_each = require('postcss-each');
+var postcss_apply = require('postcss-apply');
 var postcss_reporter = require('postcss-reporter');
 var autoprefixer = require('autoprefixer');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [postcss_import, postcss_each, postcss_custom_properties, postcss_calc, postcss_color_function, autoprefixer, postcss_reporter({
+var processors = [postcss_import, postcss_each, postcss_custom_properties, postcss_apply, postcss_calc, postcss_color_function, autoprefixer, postcss_reporter({
   clearMessages: true
 })];
 
