3a6a63e8d05861bd3f1e3fd3999b1e211ef6fa71
Ole Ersoy <ole.ersoy@gmail.com>
2017-03-02 21:45:58 -0600

    Added postcss-custom-media

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var postcss_import = require('postcss-import');
-var postcss_calc = require('postcss-calc');
-var postcss_custom_properties = require('postcss-custom-properties');
-var postcss_color_function = require('postcss-color-function');
-var postcss_each = require('postcss-each');
-var postcss_apply = require('postcss-apply');
-var postcss_reporter = require('postcss-reporter');
+var pc = require('gulp-postcss');
+var pc_import = require('postcss-import');
+var pc_calc = require('postcss-calc');
+var pc_custom_properties = require('postcss-custom-properties');
+var pc_color_function = require('postcss-color-function');
+var pc_each = require('postcss-each');
+var pc_apply = require('postcss-apply');
+var pc_reporter = require('postcss-reporter');
+var pc_custom_media = require('postcss-custom-media');
 var autoprefixer = require('autoprefixer');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [postcss_import, postcss_each, postcss_custom_properties, postcss_apply, postcss_calc, postcss_color_function, autoprefixer, postcss_reporter({
+var processors = [pc_import, pc_each, pc_custom_properties, pc_apply, pc_calc, pc_color_function, pc_custom_media, autoprefixer, pc_reporter({
   clearMessages: true
 })];
 
 gulp.task('build:css', function() {
   gulp.src(PLI.SRC_MAIN_CSS)
-    .pipe(postcss(processors))
+    .pipe(pc(processors))
     .pipe(gulp.dest(PLI.target.main.css));
 
   return gulp.src(PLI.SRC_TEST_CSS)
-    .pipe(postcss(processors))
+    .pipe(pc(processors))
     .pipe(gulp.dest(PLI.target.test.css));
 });
