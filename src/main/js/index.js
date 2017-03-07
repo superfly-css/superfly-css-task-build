f1da294a594201dd63421d8dec359025e647c352
Ole Ersoy <ole.ersoy@gmail.com>
2017-03-07 14:42:40 -0600

    Added postcss-for

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -5,6 +5,7 @@ var pc_calc = require('postcss-calc');
 var pc_custom_properties = require('postcss-custom-properties');
 var pc_color_function = require('postcss-color-function');
 var pc_each = require('postcss-each');
+var pc_for = require('postcss-for');
 var pc_apply = require('postcss-apply');
 var pc_reporter = require('postcss-reporter');
 var pc_custom_media = require('postcss-custom-media');
@@ -12,7 +13,7 @@ var autoprefixer = require('autoprefixer');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [pc_import, pc_each, pc_custom_properties, pc_apply, pc_calc, pc_color_function, pc_custom_media, autoprefixer, pc_reporter({
+var processors = [pc_import, pc_each, pc_for, pc_custom_properties, pc_apply, pc_calc, pc_color_function, pc_custom_media, autoprefixer, pc_reporter({
   clearMessages: true
 })];
 
