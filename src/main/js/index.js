9a0eaabd5022689220cc56dc28cd06de3191f014
Ole Ersoy <ole.ersoy@gmail.com>
2017-03-28 13:47:32 -0500

    Added sass color functions and updated dependencies

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,6 +4,7 @@ var pc_import = require('postcss-import');
 var pc_calc = require('postcss-calc');
 var pc_custom_properties = require('postcss-custom-properties');
 var pc_color_function = require('postcss-color-function');
+var pc_sass_color_functions = require("postcss-sass-color-functions");
 var pc_each = require('postcss-each');
 var pc_for = require('postcss-for');
 var pc_apply = require('postcss-apply');
@@ -14,7 +15,7 @@ var autoprefixer = require('autoprefixer');
 
 var PLI = require('superfly-css-pli');
 
-var processors = [pc_import, pc_each, pc_for, pc_custom_properties, pc_apply, pc_calc, pc_color_function, pc_custom_media, pc_font_magician, autoprefixer, pc_reporter({
+var processors = [pc_import, pc_each, pc_for, pc_custom_properties, pc_apply, pc_calc, pc_color_function, pc_sass_color_functions, pc_custom_media, pc_font_magician, autoprefixer, pc_reporter({
   clearMessages: true
 })];
 
